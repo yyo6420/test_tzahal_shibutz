854a012def66c1beaf819bf2a49e314eb9898048
yyo6420 <yyo6420@gmail.com>
2025-11-26 10:38:49 +0200

    class solder

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ from datetime import datetime
 import uvicorn
 import csv
 import io
-
 app = FastAPI(title = "shibutz haylim", version = "1.0.0")
 
-app.middleware("http")
-def print_middleware(request: Request, call_next):
-    print(f"Request: {request.method} {request.url.path}")
-    response = call_next(request)
-    return response
+BD_FILE = "hayal_300_no_status.sqlite"
 
