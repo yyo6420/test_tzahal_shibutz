3445c20cff8f24e2db0e4ee09ffcbab10ad7dca7
yyo6420 <yyo6420@gmail.com>
2025-11-26 10:48:13 +0200

    class room and class dorm

--- main.py
+++ main.py
@@ -5,7 +5,23 @@ from datetime import datetime
 import uvicorn
 import csv
 import io
+
 app = FastAPI(title = "shibutz haylim", version = "1.0.0")
 
 BD_FILE = "hayal_300_no_status.sqlite"
+
+class Solder(BaseModel):
+    number : int
+    first_name : str
+    last_name : str
+    gender : str
+    city : str
+    distance : int
+    status : bool
+
+class Room(BaseModel):
+    clear_beds : int
+
+class Dorm(BaseModel):
+    room_with_clear_beds : int
 
